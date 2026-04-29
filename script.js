cb17fa00782469d2bd1666e8839a9a1be2a01d6c
7-Tarun <tarunghadoria890@gmail.com>
2026-04-29 12:26:34 +0530

    Fetching weather data

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const cityInput = document.querySelector(`#city-input`);
 const searchBtn = document.querySelector(`#search-btn`);
+const apiKey = c8c53bd683d00125afc1b84a6aa3ce1d;
 
 searchBtn.addEventListener(`click`, () => {
     const cityName = cityInput.value.trim();
@@ -10,3 +11,20 @@ searchBtn.addEventListener(`click`, () => {
         console.log(`Please Enter the city!`);
     }
 });
+
+async function getData(city) {
+    try{
+        console.log(`Fetching ${city} data from the internet`);
+        const url = `https://api.openweathermap.org/data/2.5 /weather?q=${city}&appid=${c8c53bd683d00125afc1b84a6aa3ce1d}&units=metric;`
+
+        const responce = await fetch(url);
+
+        const weatherData = await responce.json();
+
+        console.log(`Successful, Here is the data: ${weatherData}`);
+    }
+
+    catch (error) {
+        console.log(`API Fail: ${error}`);
+    }
+}
